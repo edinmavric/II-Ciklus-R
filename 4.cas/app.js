25716581fa8dc749c848d1abbb0fb8a277f57394
Edin Mavric <edinmavric10@gmail.com>
2025-10-27 21:38:52 +0100

    4. Cas - Vezba

---
+++ 4.cas/app.js
@@ -0,0 +1,32 @@
+// 1. ---
+
+// <button id="countBtn">Klikni me</button>
+// <p id="count">0</p>
+
+// Svakim klikom na dugme uvećaj broj u <p> za 1.
+// (dodaj dugme „Resetuj“ koje vraća broj na 0.)
+
+// 2. ---
+
+// <ul id="list">
+//  <li>Prva stavka</li>
+// </ul>
+// <button id="addBtn">Dodaj stavku</button>
+
+// Svakim klikom dodaj novu <li> stavku sa tekstom „Nova stavka X“ (gde X raste svaki put).
+// (koristi appendChild i createElement.)
+
+// 3. ---
+
+// <p id="text">Ovo je tajni tekst</p>
+// <button id="toggleBtn">Sakrij</button>
+
+// Kada se klikne dugme, ako je tekst vidljiv – sakrij ga, a ako je sakriven – prikaži ga.
+// Promeni tekst dugmeta u „Prikazi“ ili „Sakrij“.
+
+// 4. ---
+
+// <img id="slika" src="img1.jpg" width="200">
+// <button id="changeImg">Promeni sliku</button>
+
+// Napravi niz sa 3 putanje slika i menjaš ih po redosledu svakim klikom.
